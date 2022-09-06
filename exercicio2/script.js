56cfc3d2820b122059dab278fe3566de55fd1cab
guigalvao123 <guilherme.galvaopadua@gmail.com>
2022-09-06 16:03:18 -0300

    Exercicios finalizados, e corrigidos, da aula a012

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,27 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = confirm("Voce e maior de idade?")
+let terminouEnsinoMedio = confirm ("Voce terminou o ensino medio?")
+let isCursandoOutraFaculdade = confirm ("Voce esta cursando alguma faculdade?")
 
-if(){
-
+if(idade === true){
+    console.log("Voce e maior de idade.")
+}else if(idade === false){
+    console.log("Voce nao e maior de idade.")
 }else{
-	
+    console.log("Valor indevido!")
+}
+
+if(terminouEnsinoMedio === true){
+    console.log("Voce terminou o ensino medio.")
+}else if(terminouEnsinoMedio === false){
+    console.log("Voce nao terminou o ensino medio")
+}else{
+    console.log("Valor indevido!")
+}
+
+if(isCursandoOutraFaculdade === true){
+    console.log("Voce esta cursando uma faculdade.")
+}else if(isCursandoOutraFaculdade === false){
+    console.log("Voce nao esta cursando uma faculdade.")
+}else{
+    console.log("Valor indevido!")
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,21 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt("Qual a sua nacionalidade?")
+
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+
+if(nacionalidade.toLowerCase() === "brasileira"){
+    console.log("Voce esta na lista!")
+}else if(nacionalidade.toLowerCase() === "argentina"){
+    console.log("Voce esta na lista!")
+}else if(nacionalidade.toLowerCase() === "uruguaia"){
+    console.log("Voce esta na lista!")
+}else if(nacionalidade .toLowerCase()=== "chilena"){
+    console.log("Voce esta na lista!")
+}else if (nacionalidade.toLowerCase() === "colombiana"){
+    console.log("Voce esta na lista!")
+}else{
+    console.log("Nacionalidade nao encontrada.")
+}
